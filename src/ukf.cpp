31107dadc1f34043231a207206cbbfd5700c796b
Hsin-Wen Chang <bow1226@gmail.com>
2023-02-13 19:08:24 +0800

    fix: Initialize std_a_ & std_yawdd_

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -21,6 +21,12 @@ UKF::UKF()
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
 
+  // Process noi5e standard deviation longitudinal acceleration in m/s^2
+  std_a_ = 1;
+
+  // Process noise standard deviation yaw acceleration in rad/s^2
+  std_yawdd_ = 1;
+
   /**
    * DO NOT MODIFY measurement noise values below.
    * These are provided by the sensor manufacturer.
